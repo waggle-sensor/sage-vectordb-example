4746dd9e5d784af7cd81cd431832a6588a72a314
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-02-23 11:48:38 -0600

    print certainty score in terminal

--- ImageBind_example/test.py
+++ ImageBind_example/test.py
@@ -17,6 +17,10 @@ def testText(nearText):
     # You can also analyse the result in a better way by taking a look at res.
     # Try printing res in the terminal and see what all contents it has.
     res = client.query.get("BindExample", ["text", "_additional {certainty} "]).with_near_text(nearText).do()
+    #print certainty for top 3 results
+    print(res['data']['Get']['BindExample'][0]['_additional'])
+    print(res['data']['Get']['BindExample'][1]['_additional'])
+    print(res['data']['Get']['BindExample'][2]['_additional'])
     return (res['data']['Get']['BindExample'][0]['text']),(res['data']['Get']['BindExample'][1]['text']),(res['data']['Get']['BindExample'][2]['text'])
 
 
@@ -24,4 +28,8 @@ def testImage(nearImage):
     # I am fetching top 3 results for the user, we can change this by making small 
     # altercations in this function and in upload.html file
     imres = client.query.get("BindExample", ["text", "_additional {certainty} "]).with_near_image(nearImage).do()
+    #print certainty for top 3 results
+    print(imres['data']['Get']['BindExample'][0]['_additional'])
+    print(imres['data']['Get']['BindExample'][1]['_additional'])
+    print(imres['data']['Get']['BindExample'][2]['_additional'])
     return (imres['data']['Get']['BindExample'][0]['text']),(imres['data']['Get']['BindExample'][1]['text']),(imres['data']['Get']['BindExample'][2]['text'])
