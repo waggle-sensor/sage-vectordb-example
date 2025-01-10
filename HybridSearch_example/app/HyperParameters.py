e8995b63b13d02e2c54ea7dfaf6b944c1417e668
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-01-10 15:51:31 -0600

    change type to float not list

--- HybridSearch_example/app/HyperParameters.py
+++ HybridSearch_example/app/HyperParameters.py
@@ -1,10 +1,10 @@
 #TODO: Make this into a yaml file
 
 # Weaviate module multi2vec-bind (Imagebind) weights
-textWeight = [0.4]
-imageWeight = [0.2]
-audioWeight = [0.2]
-videoWeight = [0.2]
+textWeight = 0.4
+imageWeight = 0.2
+audioWeight = 0.2
+videoWeight = 0.2
 
 # Florence 2 hyperparameters
 max_new_tokens=512 #Changed from 1024 to 512
