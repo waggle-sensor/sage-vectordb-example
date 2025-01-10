208dcbdb312c805df7b516983980de2d043b9065
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-01-10 11:35:12 -0600

    added mew HP

--- HybridSearch_example/app/HyperParameters.py
+++ HybridSearch_example/app/HyperParameters.py
@@ -11,3 +11,8 @@ max_new_tokens=512 #Changed from 1024 to 512
 early_stopping=False #Changed from False to True
 do_sample=False
 num_beams=2 #changed from 3 to 2
+
+# Hybrid Search Query hyperparameters
+response_limit=5
+query_alpha=0.5 #An alpha of 1 is a pure vector search, An alpha of 0 is a pure keyword search.
+max_vector_distance=0.4 #Maximum threshold for the vector search component
