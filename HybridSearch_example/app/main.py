28745fe597227ebbbf4f0e8d9a11d097ab700957
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-01-31 16:05:31 -0600

    drop another column

--- HybridSearch_example/app/main.py
+++ HybridSearch_example/app/main.py
@@ -114,7 +114,7 @@ def text_query(description):
                 images.append((image, f"{row['uuid']}: {row['caption']}"))
 
     #drop columns that I dont want to show
-    meta = df.drop(columns=["caption", "link", ])
+    meta = df.drop(columns=["caption", "link", "node"])
 
     # Return the images along with the entire DataFrame
     return images, meta
