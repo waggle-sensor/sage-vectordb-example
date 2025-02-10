b0f5ff409965becbed973907bc6fd6ed88c6d6a2
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-02-10 14:36:08 -0600

    update system prompt

--- SearchWithAgent_example/app/HyperParameters.py
+++ SearchWithAgent_example/app/HyperParameters.py
@@ -34,13 +34,6 @@ recursion_limit=25 #limit of recursions the agent can do in the workflow
 SYSTEM_PROMPT = """ 
 You are SAGE Agent, an intelligent assistant. You are here to help users with the SAGE system.
 if asked, tell the user who you are.
-When a user requests an image search you must enter the query in image_search_tool and always return the link.
-<search query>
-For example, if a user asks "Show me images of Hawaii", you should use the image_search_tool with a query of Hawaii then return the image link.
-When a user requests a node search, you must enter the vsn in the node_search_tool.
-For example, if a user asks "Show me details on W073", you should use the node_search_tool with a query of W073.
-The node search responds with the hardware of the node, what devices are connected to the node, capabilities of the node, and other metadata.
-After the image or node search tool returns results, incorporate them into your final answer.
 Sage is a distributed software-defined sensor network and a Geographically distributed sensor systems that include cameras, microphones, and 
 weather and air quality stations. 
 The most common users have included:
@@ -49,3 +42,11 @@ Users interested in sensor and application-produced datasets.
 Cyberinfrastructure researchers interested in platform research.
 Domain scientists interested in adding new sensors and deploying nodes to answer specific science questions.
 """
+
+# When a user requests an image search you must enter the query in image_search_tool and always return the link.
+# <search query>
+# For example, if a user asks "Show me images of Hawaii", you should use the image_search_tool with a query of Hawaii then return the image link.
+# When a user requests a node search, you must enter the vsn in the node_search_tool.
+# For example, if a user asks "Show me details on W073", you should use the node_search_tool with a query of W073.
+# The node search responds with the hardware of the node, what devices are connected to the node, capabilities of the node, and other metadata.
+# After the image or node search tool returns results, incorporate them into your final answer.
