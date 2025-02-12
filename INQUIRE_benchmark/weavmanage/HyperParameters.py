7b5fed5bbcc8ec081564ff3082d77e4ebfe4ea64
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-02-12 16:21:53 -0600

    added the schema

---
+++ INQUIRE_benchmark/weavmanage/HyperParameters.py
@@ -0,0 +1,44 @@
+'''This file contains the hyper parameters that can be changed to fine tune
+the system. 
+NOTE: Not all params have been added here. More in depth search must be 
+done to find more hyper params that can be altered'''
+#NOTE: The hyperparameters will be split up based on what microservice it corresponds to. Or I can
+#   make all the microservices apart of the same deployment so the HPs continue to be easily managed
+#   and don't get split up.
+
+from weaviate.classes.config import VectorDistances, Configure
+from weaviate.collections.classes.config_vector_index import VectorFilterStrategy
+
+#TODO: Grab a big enough sample set to test a real deployment of weaviate with Sage so you can fine tune the HPs
+#  NOTE: instead of recreating the db just update the HPs when testing
+
+# 1) Weaviate module multi2vec-bind (Imagebind) weights
+textWeight = 0.3
+imageWeight = 0.7 # Increase the weighting here so that the embedding is more influenced by the image
+audioWeight = 0 # Currently not being used
+videoWeight = 0 # Currently not being used
+
+# 2) Hierarchical Navigable Small World (hnsw) for Approximate Nearest Neighbor (ANN) hyperparamaters
+# used hsnw since it works well with bigger datasets
+# more info: https://weaviate.io/developers/weaviate/config-refs/schema/vector-index#hnsw-indexes
+# configuration tips: https://weaviate.io/developers/weaviate/config-refs/schema/vector-index#hnsw-configuration-tips
+# helpful article: https://gagan-mehta.medium.com/efficient-resource-understanding-and-planning-in-weaviate-ec673f065e86
+hnsw_dist_metric=VectorDistances.COSINE
+hnsw_ef=-1 #Balance search speed and recall, Weaviate automatically adjusts the ef value and creates a dynamic ef list when ef is set to -1
+hnsw_ef_construction=100 #Balance index search speed and build speed. Changed from 128 to 100
+hnsw_maxConnections=50 #Maximum number of connections per element. Changed from 32 to 50
+hsnw_dynamicEfMax=500 #Upper bound for dynamic ef
+hsnw_dynamicEfMin=200 #Lower bound for dynamic ef. Changed from 100 to 200
+hnsw_ef_factor=20 #This setting is only used when hnsw_ef is -1, Sets the potential length of the search list. Changed from 8 to 20
+hsnw_filterStrategy=VectorFilterStrategy.ACORN #The filter strategy to use for filtering the search results.
+hnsw_flatSearchCutoff=40000 #cutoff to automatically switch to a flat (brute-force) vector search when a filter becomes too restrictive
+hnsw_vector_cache_max_objects=1e12 #Maximum number of objects in the memory cache
+# Auto Product Quantization (PQ)
+#  https://weaviate.io/developers/weaviate/configuration/compression/pq-compression
+hnsw_quantizer=Configure.VectorIndex.Quantizer.pq(
+    training_limit=500000 #threshold to begin training
+)
+
+# 3) Weaviate module reranker-transformers (ms-marco-MiniLM-L-6-v2 Reranker Model)
+# Model info: https://huggingface.co/cross-encoder/ms-marco-TinyBERT-L-2
+# NOTE: there is no HPs I can change in this module
---
+++ INQUIRE_benchmark/weavmanage/migrations/001_create_schema.py
@@ -0,0 +1,70 @@
+from weaviate.classes.config import Configure, Property, DataType, Multi2VecField
+import HyperParameters as hp
+
+def run(client):
+    """Create the initial schema"""
+    # Create a schema to add images, audio, etc.
+    # I have used the web pages:
+    # https://weaviate.io/developers/weaviate/manage-data
+    # https://weaviate.io/developers/weaviate/model-providers/imagebind/embeddings-multimodal
+    # to get help on making a suitable schema. You can read the contents of this web page to know more.
+    # Define the schema (collection)
+    client.collections.create(
+        name="INQUIRE",
+        description="A collection to test our set up using INQUIRE",
+        properties=[
+            Property(name="inat24_image_id", data_type=DataType.NUMBER),
+            Property(name="inat24_file_name", data_type=DataType.TEXT),
+            Property(name="query", data_type=DataType.TEXT),
+            Property(name="image", data_type=DataType.BLOB),
+            Property(name="audio", data_type=DataType.BLOB),
+            Property(name="video", data_type=DataType.BLOB),
+            Property(name="caption", data_type=DataType.TEXT),  # Caption for keyword search
+            Property(name="link", data_type=DataType.TEXT),
+            Property(name="relevant", data_type=DataType.TEXT),
+            Property(name="clip_score", data_type=DataType.TEXT),
+            Property(name="supercategory", data_type=DataType.TEXT),
+            Property(name="category", data_type=DataType.TEXT),
+            Property(name="iconic_group", data_type=DataType.TEXT),
+            Property(name="inat24_species_id", data_type=DataType.NUMBER),
+            Property(name="inat24_species_name", data_type=DataType.TEXT),
+            Property(name="location_uncertainty", data_type=DataType.NUMBER),
+            Property(name="date", data_type=DataType.DATE),
+            Property(name="location", data_type=DataType.GEO_COORDINATES)
+        ],
+        vectorizer_config=[
+            Configure.NamedVectors.multi2vec_bind(
+                name="search",
+                vectorize_collection_name= False,
+                # Define fields for vectorization
+                image_fields=[
+                    Multi2VecField(name="image", weight=hp.imageWeight)
+                ],
+                text_fields=[
+                    Multi2VecField(name="caption", weight=hp.textWeight)
+                ],
+                audio_fields=[
+                    Multi2VecField(name="audio", weight=hp.audioWeight)
+                ],
+                video_fields=[
+                    Multi2VecField(name="video", weight=hp.videoWeight)
+                ],
+                vector_index_config=Configure.VectorIndex.hnsw( #https://weaviate.io/developers/weaviate/concepts/vector-index , https://weaviate.io/developers/weaviate/config-refs/schema/vector-index
+                    distance_metric=hp.hnsw_dist_metric, #works well to compare images with different attributes such as brightness levels or sizes.
+                    dynamic_ef_factor=hp.hnsw_ef_factor,
+                    dynamic_ef_max=hp.hsnw_dynamicEfMax,
+                    dynamic_ef_min=hp.hsnw_dynamicEfMin,
+                    ef=hp.hnsw_ef,
+                    ef_construction=hp.hnsw_ef_construction,
+                    filter_strategy=hp.hsnw_filterStrategy,
+                    flat_search_cutoff=hp.hnsw_flatSearchCutoff,
+                    max_connections=hp.hnsw_maxConnections,
+                    vector_cache_max_objects=int(hp.hnsw_vector_cache_max_objects),
+                    quantizer=hp.hnsw_quantizer,
+                )
+            )
+        ],
+        reranker_config=Configure.Reranker.transformers()
+    )
+
+    return
