28623c15b28b7422d5d4d3cd82ad4906cc99d55b
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-02-12 18:34:50 -0600

    added env variable INQUIRE_DATASET

--- INQUIRE_benchmark/app/main.py
+++ INQUIRE_benchmark/app/main.py
@@ -1,2 +1,8 @@
 '''This file contains the code to run the Benchmark and save the results.'''
+
+import os
+
 #TODO: left of here 02/12/2025, I need to script the queries and generate the results
+
+# Load INQUIRE benchmark dataset from Hugging Face
+INQUIRE_DATASET = os.environ.get("INQUIRE_DATASET",  "sagecontinuum/INQUIRE-Benchmark-small")
