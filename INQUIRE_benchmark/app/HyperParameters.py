74e14a3587d0979921eab8829045d2c60d374530
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-02-18 16:35:38 -0600

    feat: update hyperparameters for response limit and autocut jumps in Hybrid Search Query

--- INQUIRE_benchmark/app/HyperParameters.py
+++ INQUIRE_benchmark/app/HyperParameters.py
@@ -6,7 +6,7 @@ done to find more hyper params that can be altered'''
 from weaviate.classes.query import HybridFusion
 
 # 1) Hybrid Search Query hyperparameters
-response_limit=0 #Number of objects to return 
+response_limit=50#0 #Number of objects to return 
 query_alpha=0.4 #An alpha of 1 is a pure vector search, An alpha of 0 is a pure keyword search.
 max_vector_distance=0.4 #max accepted distance for the vector search component
 near_text_certainty=0.7 #The minimum similarity score to return. If not specified, the default certainty specified by the server is used.
@@ -18,5 +18,5 @@ avoid_concepts_force=0 #the strength to avoid the concepts
 fusion_alg=HybridFusion.RELATIVE_SCORE # RELATIVE_SCORE is default from weaviate 1.24
 # autocut limits results based on discontinuities
 # more info: https://weaviate.io/developers/weaviate/api/graphql/additional-operators#autocut
-autocut_jumps=1 #To explicitly disable autocut, set the number of jumps to 0 or a negative value
+autocut_jumps=-1#1 #To explicitly disable autocut, set the number of jumps to 0 or a negative value
 #NOTE: USE autocut_jumps OR response_limit
