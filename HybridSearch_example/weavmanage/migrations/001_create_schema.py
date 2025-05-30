e082860b8edd1ccd167430b51cfa5440ec0d50cb
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-05-29 22:18:03 -0500

    Update vectorizer configuration in schema to include 'imagebind' and add 'colbert' vector index

--- HybridSearch_example/weavmanage/migrations/001_create_schema.py
+++ HybridSearch_example/weavmanage/migrations/001_create_schema.py
@@ -34,7 +34,7 @@ def run(client):
         ],
         vectorizer_config=[
             Configure.NamedVectors.multi2vec_bind(
-                name="search",
+                name="imagebind",
                 vectorize_collection_name= False,
                 # Define fields for vectorization
                 image_fields=[
@@ -61,6 +61,23 @@ def run(client):
                     max_connections=hp.hnsw_maxConnections,
                     vector_cache_max_objects=int(hp.hnsw_vector_cache_max_objects),
                     quantizer=hp.hnsw_quantizer,
+                )
+            ),
+            Configure.NamedVectors.none(
+                name="colbert",
+                vector_index_config=Configure.VectorIndex.hnsw( #https://weaviate.io/developers/weaviate/concepts/vector-index , https://weaviate.io/developers/weaviate/config-refs/schema/vector-index
+                    distance_metric=hp.hnsw_dist_metric, #works well to compare images with different attributes such as brightness levels or sizes.
+                    dynamic_ef_factor=hp.hnsw_ef_factor,
+                    dynamic_ef_max=hp.hsnw_dynamicEfMax,
+                    dynamic_ef_min=hp.hsnw_dynamicEfMin,
+                    ef=hp.hnsw_ef,
+                    ef_construction=hp.hnsw_ef_construction,
+                    filter_strategy=hp.hsnw_filterStrategy,
+                    flat_search_cutoff=hp.hnsw_flatSearchCutoff,
+                    max_connections=hp.hnsw_maxConnections,
+                    vector_cache_max_objects=int(hp.hnsw_vector_cache_max_objects),
+                    quantizer=hp.hnsw_quantizer,
+                    multi_vector=Configure.VectorIndex.MultiVector.multi_vector()
                 )
             )
         ],
