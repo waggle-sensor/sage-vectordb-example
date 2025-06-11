c659fec871dd98b5d7b128743427cfaed4bedea2
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-06-11 16:04:21 -0500

    add Qwen2.5 prompt for inquire

--- INQUIRE_benchmark/weavloader/HyperParameters.py
+++ INQUIRE_benchmark/weavloader/HyperParameters.py
@@ -36,6 +36,27 @@ hnsw_quantizer=Configure.VectorIndex.Quantizer.pq(
 # 3) Experimental hyperparameters
 align_alpha = 0.7
 clip_alpha = 0.7
+qwen2_5_prompt="""
+role:
+You are a world-class Scientific Image Captioning Expert.
+
+context:
+You will be shown a scientific image captured by edge devices. Your goal is to analyze its content and significance in detail. 
+
+task:
+Generate exactly one scientifically detailed caption that accurately describes what is visible in the image and its scientific relevance. 
+Make it as detailed as possible. Also extract text and numbers from the images.
+
+constraints:
+- Only return:
+  1. A single caption.
+  2. a list of 15 keywords relevant to the image.
+- Do not include any additional text, explanations, or formatting.
+
+format:
+  caption: <your_scientific_caption_here>
+  keywords: <keyword1>, <keyword2>, ...
+"""
 
 # 3) Weaviate module reranker-transformers (ms-marco-MiniLM-L-6-v2 Reranker Model)
 # Model info: https://huggingface.co/cross-encoder/ms-marco-TinyBERT-L-2
