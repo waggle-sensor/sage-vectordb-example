654dcee82f8069396b3955f3ebb7acf6195725be
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-06-27 16:03:17 -0500

    updated hyperparameters for hybrid search: set response_limit to 25 and autocut_jumps to 0

--- HybridSearch_example/app/HyperParameters.py
+++ HybridSearch_example/app/HyperParameters.py
@@ -10,7 +10,7 @@ done to find more hyper params that can be altered'''
 #  NOTE: instead of recreating the db just update the HPs when testing
 
 # 1) Hybrid Search Query hyperparameters
-response_limit=0 #Number of objects to return 
+response_limit=25 #Number of objects to return 
 query_alpha=0.4 #An alpha of 1 is a pure vector search, An alpha of 0 is a pure keyword search.
 max_vector_distance=0.4 #max accepted distance for the vector search component
 near_text_certainty=0.7 #The minimum similarity score to return. If not specified, the default certainty specified by the server is used.
@@ -19,8 +19,9 @@ concepts_to_avoid=["police", "gun"] # Concepts to avoid
 avoid_concepts_force=0 #the strength to avoid the concepts
 # autocut limits results based on discontinuities
 # more info: https://weaviate.io/developers/weaviate/api/graphql/additional-operators#autocut
-autocut_jumps=1 #To explicitly disable autocut, set the number of jumps to 0 or a negative value
+autocut_jumps=0 #To explicitly disable autocut, set the number of jumps to 0 or a negative value
 #NOTE: USE autocut_jumps OR response_limit
+#NOTE: autocut_jumps doesn't work that well with DFN5B-CLIP-ViT-H-14-378
 hybrid_weight=0.7 #The weight of the hybrid search component in the unified score for hybrid colbert blend.
 colbert_weight=0.3 #The weight of the colbert search component in the unified score for hybrid colbert blend.
 hybrid_colbert_blend_top_k=25 #The number of top results to return from the hybrid colbert blend search.
