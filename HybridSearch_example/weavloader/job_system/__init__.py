70bd9f25c2b089791cd1d7186555a046cd3abeac
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-10-17 15:05:24 -0500

    Refactor import statement in job_system to use 'app' instead of 'celery_app'

--- HybridSearch_example/weavloader/job_system/__init__.py
+++ HybridSearch_example/weavloader/job_system/__init__.py
@@ -1,5 +1,5 @@
 '''Job System package for Weavloader'''
 
-from .tasks import app as celery_app, process_image_task, monitor_data_stream, cleanup_failed_tasks, reprocess_dlq_tasks, dlq_health_check
+from .tasks import app, process_image_task, monitor_data_stream, cleanup_failed_tasks, reprocess_dlq_tasks, dlq_health_check
 
-__all__ = ['celery_app', 'process_image_task', 'monitor_data_stream', 'cleanup_failed_tasks', 'reprocess_dlq_tasks', 'dlq_health_check']
+__all__ = ['app', 'process_image_task', 'monitor_data_stream', 'cleanup_failed_tasks', 'reprocess_dlq_tasks', 'dlq_health_check']
