2c40b9e215ab641d704ddff53288063ccdbc7d9a
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-10-17 16:54:16 -0500

    Enhance logging configuration in __main__.py to include timestamp and message format for improved log readability

--- HybridSearch_example/weavloader/metrics/__main__.py
+++ HybridSearch_example/weavloader/metrics/__main__.py
@@ -7,5 +7,10 @@ import logging
 from .server import start_metrics_server
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(message)s",
+        datefmt="%Y/%m/%d %H:%M:%S",
+    )
+    logger = logging.getLogger(__name__)
     start_metrics_server()
