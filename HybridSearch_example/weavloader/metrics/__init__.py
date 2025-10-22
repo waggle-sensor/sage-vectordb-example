6858cf99d72521f9288fc3c4ac7c798d5852d33d
FranciscoLozCoding <francisco.lozano33@outlook.com>
2025-10-22 13:36:10 -0500

    fixed wrong import function

--- HybridSearch_example/weavloader/metrics/__init__.py
+++ HybridSearch_example/weavloader/metrics/__init__.py
@@ -1,6 +1,6 @@
 '''Metrics package for Weavloader'''
 
-from .metrics import metrics, get_metrics, get_metrics_registry
+from .metrics import metrics, get_metrics
 from .server import start_metrics_server
 
-__all__ = ['metrics', 'get_metrics', 'get_metrics_registry', 'start_metrics_server']
+__all__ = ['metrics', 'get_metrics', 'start_metrics_server']
